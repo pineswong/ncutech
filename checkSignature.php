da3623341d766e24a52b72be6256cbf06c4e2587
pineswong <pineswong@163.com>
2015-08-06 22:36:29 +0800

    signature2

--- checkSignature.php
+++ checkSignature.php
@@ -6,24 +6,3 @@
  * @version $Id$
  */
 
-// 定义Token
-define('TOKEN', 'ncutech');
-
-// 验证
-function checkSignature($signature, $timestamp, $nonce) {
-	// 获取数据
-	$token = TOKEN;
-
-	// 处理数据
-	$tmpArr = array($token, $timestamp, $nonce);
-	sort($tmpArr, SORT_STRING);
-	$tmpStr = implode($tmpArr);
-	$tmpStr = sha1($tmpStr);
-
-	// 处理返回
-	if ($tmpStr != $signature) {
-		return false;
-	}
-
-	return true;
-}
--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php 
 
 	// echo 'NcuTech' 
-	// 引入函数
-	require './checkSignature.php';
 
+	// 定义Token
+	define('TOKEN', 'ncutech');
+
+	// 验证函数
+	function checkSignature($signature, $timestamp, $nonce) {
+		// 获取数据
+		$token = TOKEN;
+
+		// 处理数据
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode('', $tmpArr);
+		$tmpStr = sha1($tmpStr);
+
+		// 处理返回
+		if ($tmpStr != $signature) {
+			return false;
+		}
+
+		return true;
+	}
 
 	// 获取数据
 	$signature = $_GET['signature'];
