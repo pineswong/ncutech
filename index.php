b47c6bdb34519e2331bf25a27692d58f0b52ce4a
pineswong <pineswong@163.com>
2015-08-06 23:20:22 +0800

    weichat link

--- index.php
+++ index.php
@@ -6,8 +6,27 @@
  * @version $Id$
  */
 
-	// 引入函数
-	require './checkSignature.php';
+	// 定义Token
+	define('TOKEN', 'ncutech');
+
+	// 验证函数
+	function checkSignature($signature, $timestamp, $nonce) {
+		// 获取数据
+		$token = TOKEN;
+
+		// 处理数据
+		$tmpArr = array($token, $timestamp, $nonce);
+		$tmpArr = sort($tmpArr, SORT_STRING);
+		$tmpStr = implode('', $tmpArr);
+		$tmpStr = sha1($tmpStr);
+
+		// 处理返回
+		if ($tmpStr != $signature) {
+			return false;
+		}
+
+		return true;
+	}
 
 	// 获取数据
 	$signature = $_GET['signature'];
@@ -15,7 +34,7 @@
 	$nonce = $_GET['nonce'];
 	$echostr = $_GET['echostr'];
 
-	echo $echostr;
+//	echo $echostr;
 
 	// 验证Token
 	if (checkSignature($signature, $timestamp, $nonce)) {
