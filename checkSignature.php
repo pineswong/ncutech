4d2aa18da6a795c5c0bea50d75b35849218d258f
pineswong <pineswong@163.com>
2015-08-06 23:11:29 +0800

    weichat link

--- checkSignature.php
+++ checkSignature.php
@@ -6,3 +6,24 @@
  * @version $Id$
  */
 
+	// 定义Token
+	define('TOKEN', 'ncutech');
+
+	// 验证函数
+	function checkSignature($signature, $timestamp, $nonce) {
+		// 获取数据
+		$token = TOKEN;
+
+		// 处理数据
+		$tmpArr = array($token, $timestamp, $nonce);
+		$tmpArr = sort($tmpArr, SORT_STRING);
+		$tmpStr = implode('', $tmpArr);
+		$tmpStr = sha1($tmpStr);
+
+		// 处理返回
+		if ($tmpStr != $signature) {
+			return false;
+		}
+
+		return true;
+	}
--- index.php
+++ index.php
@@ -1,25 +1,13 @@
 <?php 
-	// 定义Token
-	define('TOKEN', 'ncutech');
+/**
+ * index.php
+ * @authors Pines Wong (pineswong@163.com)
+ * @date    2015-08-06 22:10:51
+ * @version $Id$
+ */
 
-	// 验证函数
-	function checkSignature($signature, $timestamp, $nonce) {
-		// 获取数据
-		$token = TOKEN;
-
-		// 处理数据
-		$tmpArr = array($token, $timestamp, $nonce);
-		$tmpArr = sort($tmpArr, SORT_STRING);
-		$tmpStr = implode('', $tmpArr);
-		$tmpStr = sha1($tmpStr);
-
-		// 处理返回
-		if ($tmpStr != $signature) {
-			return false;
-		}
-
-		return true;
-	}
+	// 引入函数
+	require './checkSignature';
 
 	// 获取数据
 	$signature = $_GET['signature'];
@@ -31,5 +19,3 @@
 	if (checkSignature($signature, $timestamp, $nonce)) {
 		echo $echostr;
 	}
-
-?>
