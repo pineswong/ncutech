88f52aca20d1705387a4936637a0fe06bad1fe99
pineswong <pineswong@163.com>
2015-08-06 22:25:33 +0800

    微信接入

---
+++ checkSignature.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * checkSignature.php
+ * @authors Pines Wong (pineswong@163.com)
+ * @date    2015-08-06 22:10:51
+ * @version $Id$
+ */
+
+// 定义Token
+define('TOKEN', 'ncutech');
+
+// 验证
+function checkSignature($signature, $timestamp, $nonce) {
+	// 获取数据
+	$token = TOKEN;
+
+	// 处理数据
+	$tmpArr = array($token, $timestamp, $nonce);
+	sort($tmpArr, SORT_STRING);
+	$tmpStr = implode($tmpArr);
+	$tmpStr = sha1($tmpStr);
+
+	// 处理返回
+	if ($tmpStr != $signature) {
+		return false;
+	}
+
+	return true;
+}
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo 'NcuTech' ?>
+<?php 
+
+	// echo 'NcuTech' 
+	// 引入函数
+	require './checkSignature.php';
+
+
+	// 获取数据
+	$signature = $_GET['signature'];
+	$timestamp = $_GET['timestamp'];
+	$nonce = $_GET['nonce'];
+	$echostr = $_GET['echostr'];
+
+	// 验证Token
+	if (checkSignature()) {
+		echo $echostr;	
+	}
+
+?>
