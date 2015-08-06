20cfaf6170eab55798934d591a3fcb234a8e3ee0
pineswong <pineswong@163.com>
2015-08-06 23:22:00 +0800

    weichat link

--- index.php
+++ index.php
@@ -9,24 +9,7 @@
 	// 定义Token
 	define('TOKEN', 'ncutech');
 
-	// 验证函数
-	function checkSignature($signature, $timestamp, $nonce) {
-		// 获取数据
-		$token = TOKEN;
-
-		// 处理数据
-		$tmpArr = array($token, $timestamp, $nonce);
-		$tmpArr = sort($tmpArr, SORT_STRING);
-		$tmpStr = implode('', $tmpArr);
-		$tmpStr = sha1($tmpStr);
-
-		// 处理返回
-		if ($tmpStr != $signature) {
-			return false;
-		}
-
-		return true;
-	}
+	$token = TOKEN;
 
 	// 获取数据
 	$signature = $_GET['signature'];
@@ -34,9 +17,13 @@
 	$nonce = $_GET['nonce'];
 	$echostr = $_GET['echostr'];
 
-//	echo $echostr;
+	// 处理数据
+	$tmpArr = array($token, $timestamp, $nonce);
+	$tmpArr = sort($tmpArr, SORT_STRING);
+	$tmpStr = implode('', $tmpArr);
+	$tmpStr = sha1($tmpStr);
 
-	// 验证Token
-	if (checkSignature($signature, $timestamp, $nonce)) {
+	// 处理返回
+	if ($tmpStr == $signature) {
 		echo $echostr;
 	}
